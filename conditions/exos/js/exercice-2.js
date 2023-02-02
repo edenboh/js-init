95d99f1b2816c9e6d4ab600996f949c4c065478e
JPechberty <j.pechberty@gmail.com>
2023-02-03 00:26:36 +0100

    init

---
+++ conditions/exos/js/exercice-2.js
@@ -0,0 +1,6 @@
+console.log("Script condition-1 chargé !")
+
+//Décommentez les lignes ci-dessous
+// const password = prompt('Entrez un mot de passe')
+
+// Ecrire votre code ici, afficher si le résultat est pair ou impair à l'aide de console.log('votre réponse')
