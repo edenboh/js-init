a4093c4f273380d3ff412ce7ab0397da14dae937
JPechberty <Ninposhine313>
2023-03-14 12:41:48 +0100

    Ajout exercice visualisation DOM

---
+++ dom/exos/affichage/view-dom.js
@@ -0,0 +1,20 @@
+console.log('Script view DOM data loaded.')
+
+let student = {
+    classe:"BTS SIO 1 ALT",
+    prenom:"Mathieu",
+    nom:"DURAND",
+    adresse:"34 avenue du plateau",
+    cp:"94220",
+    ville:"CHARENTON LE PONT",
+    email:"m.durand@mail.fr",
+    telephone:"06-23-87-90-45",
+    releveNotes:{
+        mathematiques:[12,10],
+        cybersecurite:[13,10],
+        cejm:[16,10],
+        atpro:[12,10],
+        anglais:[16,10],
+    }
+}
+
